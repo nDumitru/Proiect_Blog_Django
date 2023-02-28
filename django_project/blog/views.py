dba18ce7c83927927e5a0b34da59ebf097e7b98b
nDumitru <nechitadumitru@gmail.com>
2023-03-01 00:31:54 +0200

    2023/03/01_00:31

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
 
-# Create your views here.
+posts = [
+    {
+        'author': 'nDumitru',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'February 28, 2023'
+    },
+    {
+        'author': 'nDumitru',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'February 28, 2023'
+    }
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html')
